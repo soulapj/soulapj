306d2eddf2ef43c82b93d59e6a14fe6e5c9a9066
soulapj <127118860+soulapj@users.noreply.github.com>
2024-11-20 08:39:43 +0100

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,186 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    categorieProjet: [
+      {
+        id: 1,
+        name: "First Tier",
+        description: "Pour projets a mettre en avant"
+      },
+      {
+        id: 2,
+        name: "Second Tier",
+        description: "Pour les autres projets"
+      },
+      {
+        id: 3,
+        name: "Third Tier",
+        description: "Pour exercices et petite tech"
+      },
+    ],
+    projets: [
+      {
+        id: 3,
+        name: "Webwares",
+        image: require("@/assets/previews/ecf3.avif"),
+        description: "Crée dans le cadre d'un ECF, front et back du site de vente de meuble pour professionnels. Compte Admin SIRET: 75008 mdp: motdepasseB ",
+        visualCategory: 1,
+        techCategory: "VueJS",
+        lien: "https://webwares.netlify.app/",
+        git: "https://github.com/soulapj/WebWares.git"
+      },
+      {
+        id: 1,
+        name: "Georgia Restaurant",
+        image: require("@/assets/previews/ecf1.avif"),
+        description: "Crée dans le cadre d'un ECF, front du site d'un restaurant avec version desktop et mobile.",
+        visualCategory: 1,
+        techCategory: "Html / CSS",
+        lien: "https://eventgrids-exo.netlify.app/",
+        git: "https://github.com/soulapj/aoutsolo.git"
+      },
+      {
+        id: 2,
+        name: "Event Grids",
+        image: require("@/assets/previews/ecf2.avif"),
+        description: "Crée dans le cadre d'un ECF, front du site d'organisation d'évenements avec version desktop et mobile. Travail de Groupe",
+        visualCategory: 1,
+        techCategory: "Html / CSS",
+        lien: "https://georgiarestaurant-exo.netlify.app/",
+        git: "https://github.com/soulapj/aoutgroup.git"
+      },
+
+      {
+        id: 4,
+        name: "Menu",
+        image: require("@/assets/previews/vue1.avif"),
+        description: "Front pour menu de fastfood intéractif",
+        visualCategory: 2,
+        techCategory: "VueJS",
+        lien: "https://celadon-tapioca-c30810.netlify.app/",
+        git: "https://github.com/soulapj/menu"
+      },
+      {
+        id: 5,
+        name: "Liste Produits",
+        image: require("@/assets/previews/vue2.avif"),
+        description: "Back pour création, modification et suppression de produits",
+        visualCategory: 2,
+        techCategory: "VueJS",
+        lien: "https://candid-croissant-3a8982.netlify.app/",
+        git: "https://github.com/soulapj/listeProduit"
+      },
+      {
+        id: 6,
+        name: "To-Do List",
+        image: require("@/assets/previews/vue3.avif"),
+        description: "Liste de tâche à faire intéractive avec ajout, complété et suppression.",
+        visualCategory: 2,
+        techCategory: "VueJS",
+        lien: "https://rad-parfait-44c9b2.netlify.app/",
+        git: "https://github.com/soulapj/toDoList"
+      },
+      {
+        id: 7,
+        name: "Jeu question",
+        image: require("@/assets/previews/vue4.avif"),
+        description: "Jeu de culture générale qui comptabilise les points contre l'ordinateur. Utilise Axios et la base de donnée d'OpenTrivia",
+        visualCategory: 2,
+        techCategory: "VueJS",
+        lien: "https://jeuquestion.netlify.app/",
+        git: "https://github.com/soulapj/questionnaire"
+      },
+      {
+        id: 8,
+        name: "Panier Produit",
+        image: require("@/assets/previews/vue5.avif"),
+        description: "Liste de Produit et Panier intéractif",
+        visualCategory: 2,
+        techCategory: "VueJS",
+        lien: "https://panierproduit.netlify.app/",
+        git: "https://github.com/soulapj/produitpanier"
+      },
+      {
+        id: 9,
+        name: "Dés",
+        image: require("@/assets/previews/js1.avif"),
+        description: "Dé en 3d pivotable. Réalisé en utilisant ThreeJS",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://deft-treacle-2d1371.netlify.app/",
+        git: "https://github.com/soulapj/dice"
+      },
+      {
+        id: 10,
+        name: "Drawing Canvas",
+        image: require("@/assets/previews/js2.avif"),
+        description: "Tableau blanc sur lequel on peut dessiner en noir et effaçable.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://peaceful-naiad-a54ccf.netlify.app/",
+        git: "https://github.com/soulapj/drawing_canvas"
+      },
+      {
+        id: 11,
+        name: "IMC",
+        image: require("@/assets/previews/js3.avif"),
+        description: "Calcul de l'IMC.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://delightful-tanuki-a663f8.netlify.app/",
+        git: "https://github.com/soulapj/IMC"
+      },
+      {
+        id: 12,
+        name: "Liste Utilisateur",
+        image: require("@/assets/previews/js4.avif"),
+        description: "Liste Utilisateur filtrable.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://sensational-khapse-5920e8.netlify.app/",
+        git: "https://github.com/soulapj/ListeUsers"
+      },
+      {
+        id: 13,
+        name: "Liste Livres",
+        image: require("@/assets/previews/js5.avif"),
+        description: "Liste de livres. Option d'ajout, suppression et modification des livres.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://fastidious-peony-3eca02.netlify.app/",
+        git: "https://github.com/soulapj/listeCrud"
+      },
+      {
+        id: 14,
+        name: "Meteo locale",
+        image: require("@/assets/previews/js6.avif"),
+        description: "Bouton qui, lorsque l'on entre le nom d'une ville, donne la météo actuelle.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://snazzy-sorbet-f68ea3.netlify.app/",
+        git: "https://github.com/soulapj/meteoGeo"
+      },
+      {
+        id: 15,
+        name: "Shifumi",
+        image: require("@/assets/previews/js7.avif"),
+        description: "Jeu de Pierre-Papier-Ciseau contre l'ordinateur.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://silver-otter-a01d12.netlify.app/",
+        git: "https://github.com/soulapj/Shifumi"
+      },
+      {
+        id: 16,
+        name: "Validation Form",
+        image: require("@/assets/previews/js8.avif"),
+        description: "Formulaire d'inscription en Javascript.",
+        visualCategory: 3,
+        techCategory: "Javascript",
+        lien: "https://willowy-sunburst-2b3aca.netlify.app/",
+        git: "https://github.com/soulapj/validationForm"
+      },
+    ]
   },
   getters: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,298 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="body">
+      <div class="categories">
+        <h1 class="NOM front">SOULA</h1>
+        <h2 class="PRENOM front">Pierre-Jean</h2>
+        <div class="bandeau1">
+          <div class="poste">
+          <a class="poste1">Développeur</a><br>
+          <a class="poste2">Web et Web Mobile</a>
+        </div>
+        </div>
+        <div class="bandeau2"></div>        
+        <div class="contacts">
+          <a href="mailto:soulapj@gmail.com">soulapj@gmail.com</a><br>
+          <a>0628905348</a>
+        </div>
+        <div class="grandProjetsOrg">
+          <div v-for="(projet, index) in projets" :key="index">
+            <div v-if="projet.visualCategory === 1" class="grandProjets">
+              <img :src="projet.image" class="grandImage"/>
+              <div class="options">
+                <h3>{{ projet.name }}</h3>
+                <a> {{ projet.description }} </a><br><br>
+                <a> {{ projet.techCategory }} </a><br>
+                <div class="buttongroup">
+                  <a class="button site" :href="projet.lien" target="_blank">Voir le site</a>
+                  <a class="button git" :href="projet.git" target="_blank">GIT</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="Texte">
+          <p>A 25 ans, je suis actuellement en reconversion professionelle dans le milieu du développement web. Je suis en formation à l'AFPA Digital qui m'a permis d'acquérir les bases du HTML, CSS, SCSS, Javascript, Vue, PHP mais également la gestion de base de données SQL, NoSQL et MongoDB. J'ai également eu l'opportunité de travailler en groupe en utilisant des outils tels que Git Hub et d'être en charge des merges et des commits.</p>
+        </div>
+
+        <div class="moyenProjetsOrg">
+          <div v-for="(projet, index) in projets" :key="index">
+            <div v-if="projet.visualCategory === 2" class="moyenProjets">
+              <img :src="projet.image" class="moyenImage"/>
+              <div class="options">
+                <h3>{{ projet.name }}</h3>
+                <a> {{ projet.description }} </a><br><br>
+                <a> {{ projet.techCategory }} </a><br>
+                <div class="buttongroup">
+                  <a class="button site" :href="projet.lien" target="_blank">Voir le site</a>
+                  <a class="button git" :href="projet.git" target="_blank">GIT</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="Texte">
+          <p class="Textecoté">Intéressés par ce que vous avez-vu jusqu'à present? Contactez-moi!</p>
+          <a href="mailto:soulapj@gmail.com">mail: soulapj@gmail.com</a> <br>
+          <a>téléphone: 06 28 90 53 48</a>
+        </div>
+
+        <div class="moyenProjetsOrg">
+          <div v-for="(projet, index) in projets" :key="index">
+            <div v-if="projet.visualCategory === 3" class="moyenProjets">
+              <img :src="projet.image" class="moyenImage"/>
+              <div class="options">
+                <h3>{{ projet.name }}</h3>
+                <a> {{ projet.description }} </a><br><br>
+                <a> {{ projet.techCategory }} </a><br>
+                <div class="buttongroup">
+                  <a class="button site" :href="projet.lien" target="_blank">Voir le site</a>
+                  <a class="button git" :href="projet.git" target="_blank">GIT</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState} from "vuex";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  computed: {
+    ...mapState(["categorieProjet", "projets"])
   }
 }
+
+
 </script>
+
+
+<style>
+  .body {
+    height: auto;
+    width: 55%;
+    position: relative;
+    background-color: rgb(255, 255, 255);
+    left:50%;
+    transform: translateX(-50%);
+    z-index: 10;
+    box-shadow: 0px 0px 50px -25px #2b3d4f;
+    display: flex;
+    justify-content: center;
+    
+    /* border: #2b3d4f solid 3px; */
+    }
+
+  .bandeau1 {
+    background-color: rgb(165, 216, 255);
+    position: absolute;
+    left: 50%;
+    top: 5vh;
+    transform: translateX(-33%);
+    width: 1000px;
+    height: 350px;
+    box-shadow: 0px 0px 50px -30px #2c3e50;
+    padding: 10px;
+    align-content: center;
+    justify-items: left;
+    z-index:-1;
+    }
+
+  .NOM {
+    position: absolute;
+    font-size: 110px;
+    font-weight: 900;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    left: -60px;
+    top: 130px;
+  }
+
+  .PRENOM {
+    position: absolute;
+    font-size: 100px;
+    top:110px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    left: -60px;
+    top: 220px;
+  }
+
+  .poste {
+    z-index: 3;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+    color: rgb(225, 242, 255);
+  }
+
+  .poste1{
+    position: relative;
+    top:-15px;
+    left: -5px;
+    font-size: 185px;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-weight: 900;
+    margin: -30px
+  }
+  .poste2{
+    position: relative;
+    text-wrap: nowrap;
+    top:-20px;
+    font-size: 120px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-weight: 900;
+    margin: -30px
+  }
+
+  .contacts {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-self: end;
+    top: 385px;
+    left: 48vw;
+    font-size: 30px;
+    font-weight: 900;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: #2b3d4f;
+  }
+  .contacts a {
+    color: #2b3d4f;
+    text-decoration: none;
+    margin: -10px;
+  }
+
+  .bandeau2 {
+    border: #2c3e50 7px dashed;
+    position: absolute;
+    left: -10%;
+    top: 20.5vh;
+    width: 350px;
+    height: 25vh;
+    padding: 10px;
+    align-content: center;
+    justify-items: left;
+    z-index: -2;
+    }
+
+  .Texte {
+    background-color: rgb(165, 216, 255) ;
+    position:relative;
+    display: flex;
+    z-index: 3;
+    padding: 20px 70px 30px 70px;
+    font-size: 14px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    color: #2b3d4f;
+    margin-bottom: 25px;
+    transform: scale(105%);
+    flex-direction: column;
+  }
+
+
+  .Texte a {
+    color: #2b3d4f;
+    text-decoration: none;
+    font-size: 20px;
+    margin: -5px;
+  }
+
+    
+  .grandProjetsOrg {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 50vh;
+  }
+  
+  .grandProjets {
+    /* border: #2c3e50 solid 5px; */
+    display: flex;
+    border-radius: 25px;
+    width: 850px;
+    height: 325px;
+    box-shadow: 0px 0px 50px -20px #2c3e50;
+    margin: 40px;
+    background-color: white;
+  }
+
+  .grandProjets .grandImage {
+    width: 550px;
+    border-radius: 25px 0 0 25px;
+    object-fit: cover;
+  }
+
+  .grandProjets .options {
+    padding: 40px;
+  }
+
+  .grandProjets .buttongroup {
+    margin-top: 40px
+  }
+
+  .button {
+    background-color: #2c3e50;
+    padding: 10px;
+    margin: 5px;
+    color: #ffffff;
+    border-radius: 10px;
+    text-decoration: none;
+  }
+
+  .moyenProjetsOrg {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .moyenProjets {
+    display: flex;
+    flex-direction: column;
+    border-radius: 25px;
+    width: 250px;
+    height: auto;
+    box-shadow: 0px 0px 50px -25px #2c3e50;
+    margin: 10px 25px 25px 25px;
+    padding-bottom: 25px;
+    background-color: white;
+    
+  }
+
+  .moyenImage {
+    height: 175px;
+    border-radius: 25px 25px 0 0;
+    object-fit: cover;
+    border-bottom: #2c3e50 3px solid;
+  }
+
+  .moyenProjets .buttongroup {
+    margin-top: 4vh
+  }
+
+</style>
+
